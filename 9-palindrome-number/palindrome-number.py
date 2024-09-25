9be190a806136872a599203f98579b5835d5df3a
OnlyJustTom <116193649+OnlyJustTom@users.noreply.github.com>
2024-09-25 10:06:48 +0100

    Time: 48 ms (79.96%) | Memory: 16.7 MB (6.64%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if(x < 0):
+            return False
+        else:
+            reverse = 0
+            y = x
+            while y != 0:    
+                digit = y % 10
+                reverse = reverse * 10 + digit
+                y //= 10
+            return reverse == x
